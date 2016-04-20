3a214e13f8cd208c401718c1c650d02bf2b03e49
Yan Mihailov <yanmhlv@gmail.com>
2016-04-20 17:41:26 +0300

    questions from chapter 1; sum and product procedure

---
+++ chapter_1.rkt
@@ -0,0 +1,59 @@
+(define (cube x) (* x x x))
+
+; (define (sum-integers a b)
+;     (if (> a b)
+;         0
+;         (+ a (sum-integers (+ a 1) b))))
+
+; (define (sum-cubes a b)
+;     (if (> a b)
+;         0
+;         (+ (cube a) (sum-cubes (+ a 1) b))))
+
+; (define (pi-sum a b)
+;     (if (> a b)
+;         0
+;         (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+(define (sum term a next b)
+    (if (> a b)
+        0
+        (+ (term a)
+            (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+    (sum cube a inc b))
+
+(define (identity x) x)
+(define (sum-integers a b)
+    (sum identity a inc b))
+
+(define (pi-sum a b)
+    (define (pi-term x)
+        (/ 1.0 (* x (+ x 2))))
+    (define (pi-next x)
+        (+ x 4))
+    (sum pi-term a pi-next b))
+
+(define (integral f a b dx)
+    (define (add-dx x) (+ x dx))
+    (* (sum f (+ a (/ dx 2)) add-dx b) dx))
+
+(define (itersum term a next b)
+    (define (iter a result)
+        (if (> a b)
+            result
+            (iter (next a) (+ result (term a)))))
+    (iter a 0))
+
+(define (product term a next b)
+    (if (> a b)
+        1
+        (* (term a) (product term (next a) next b))))
+
+(define (factorial n)
+    (define (identity x) x)
+    (define (next x) (+ x 1))
+    (product identity 1 next n))
+(factorial 6)
