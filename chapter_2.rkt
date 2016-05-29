f914272874af96607d588f0c50424d5418aeea70
Yan Mihailov <yanmhlv@gmail.com>
2016-05-29 23:20:44 +0300

    ex 2.2

--- chapter_2.rkt
+++ chapter_2.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
 
+(define (average a b) (/ (+ a b) 2))
+
 (define (gcd a b)
   (if (= b 0) a
       (gcd b (remainder a b))))
@@ -44,8 +46,28 @@
 (define (equal-rat? x y)
   (= (* (numer x) (denom y)) (* (numer y) (denom x))))
 
-(print-rat (make-rat 1 2))
-(print-rat (make-rat -1 -2))
-(print-rat (make-rat -1 2))
-(print-rat (make-rat 1 -2))
 (print-rat (make-rat 10 -20))
+
+; ex 2.2
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (make-segment p1 p2) (cons p1 p2))
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
+
+(define (mindpoint-segment s)
+  (make-point (average (x-point (start-segment s)) (x-point (end-segment s)))
+              (average (y-point (start-segment s)) (y-point (end-segment s)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(print-point (mindpoint-segment (make-segment (make-point -1 2) (make-point 3 -6))))
+(print-point (mindpoint-segment (make-segment (make-point 6.4 3) (make-point -10.7 4))))
