e34124c5684b940c815e5ef3338a4885c58c444b
Yan Mihailov <yanmhlv@gmail.com>
2016-06-28 14:47:50 +0300

    wip

---
+++ chapter_2/list_functions_2.rkt
@@ -0,0 +1,37 @@
+(define (filter predicate sequence)
+  (cond ((null? sequence) null)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) null sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+
+(define (fold-right op init sequence)
+  (accumulate op init sequence))
+
+(define (fold-left op init sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+
+(length (list 1 2 3 989))
+(map (lambda (x) (* x x)) (list 1 2 3 4 5 6))
+(length (list 1 2 3 4 5 6))
