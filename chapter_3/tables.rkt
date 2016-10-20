4dbe42df919589a76321b13b70941a6a64199d89
Yan Mihailov <yanmhlv@gmail.com>
2016-10-20 11:44:34 +0300

    added memoize procedure

--- chapter_3/tables.rkt
+++ chapter_3/tables.rkt
@@ -77,3 +77,26 @@
 (define operation-table (make-table))
 (define get (operation-table 'lookup-proc))
 (define put (operation-table 'insert-proc!))
+
+
+; ex 3.27
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else
+         (+ (fib (- n 1)) (fib (- n 2))))))
+(define memo-fib
+  (memoize (lambda (n)
+             (cond ((= n 0) 0)
+                   ((= n 1) 1)
+                   (else
+                    (+ (memo-fib (- n 1))
+                       (memo-fib (- n 2))))))))
+(define (memoize f)
+  (let ((table (make-table)))
+    (lambda (x)
+      (let ((previously-computed-result (lookup x table)))
+        (or previously-computed-result
+            (let ((result (f x)))
+              (insert! x result table)
+              result))))))
