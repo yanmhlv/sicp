d9173f9c2fc6359afc0c2b9f3d32b0111ba7c9ee
Yan Mihailov <yanmhlv@gmail.com>
2016-10-24 16:33:29 +0300

    declarative and imperative factorial

---
+++ chapter_3/factorial.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+; declarative factorial
+(define (factorial n)
+  (define (iter product counter)
+    (if (> counter n)
+        product
+        (iter (* counter product) (+ counter 1))))
+  (iter 1 1))
+; imperative factorial
+(define (factorial n)
+  (let ((product 1)
+        (counter 1))
+    (define (iter)
+      (if (> counter n)
+          product
+          (begin (set! product (* counter product))
+                 (set! counter (+ counter 1))
+                 (iter))))
+    (iter)))
